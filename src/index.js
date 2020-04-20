c3987262721d24d03642c8271b3a101e13d78f76
CharithKularatna <charithkularatna@gmail.com>
2020-04-20 22:58:04 +0530

    Thunk Installed, Store Initiated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import authReducer from './store/reducers/auth'
 
+const store = createStore(authReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+        
 );
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,13 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    form: null
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        default: return state;
+    }
+};
+
+export default reducer;

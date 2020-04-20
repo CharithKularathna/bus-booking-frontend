c8ca946b4452dd22639501d5e9f480e02602ee68
CharithKularatna <charithkularatna@gmail.com>
2020-04-20 23:21:54 +0530

    Redux, Redux DevTools Installed. Store Fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import authReducer from './store/reducers/auth'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(authReducer, composeEnhancers(
     applyMiddleware(thunk)
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,7 +1,8 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    form: null
+    token: null,
+    userID: null
 };
 
 const reducer = ( state = initialState, action ) => {

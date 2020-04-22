7f7cbdd805ca42ff6d00fbf8e1068b233938a41d
CharithKularatna <charithkularatna@gmail.com>
2020-04-22 23:45:06 +0530

    Dashboard Content file removed, Sidebar Center

--- src/containers/Passenger/Dashboard/Dashboard.js
+++ src/containers/Passenger/Dashboard/Dashboard.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import Content from './DashBoardContent/DashboardContent'
-import Sidebar from '../../../components/Nav/Sidebar/Sidebar';
 import classes from './Dashboard.css'
 
 class Dashboard extends Component {

3e24543240ccae4414bbe48198e4f115caab98b3
CharithKularatna <charithkularatna@gmail.com>
2020-05-12 21:40:38 +0530

    First Unit TEsts passed

--- src/components/Nav/Navbar/Navbar.test.js
+++ src/components/Nav/Navbar/Navbar.test.js
@@ -1,14 +1,24 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adpater-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 import NavBar from './Navbar'
-import shallow from 'enzyme/build/shallow';
 
 describe('<NavBar />',() => {
     let wrapper = null;
     beforeEach(() => {
         wrapper = shallow(<NavBar />)
     })
-    
+    it("Should have 3 NavLinks if not Authenticated",()=>{
+        expect(wrapper.find("li")).toHaveLength(3)
+    })
+
+    it("Should have 4 NavLinks if Authenticated",()=>{
+        wrapper = shallow(<NavBar isAuthenticated />)
+        expect(wrapper.find("li")).toHaveLength(3)
+    })
+
 })

0d2e5b6ae331ac421c6ada53117796245aa15500
CharithKularatna <charithkularatna@gmail.com>
2020-05-21 09:39:40 +0530

    Footer Logo, Bsic Structure Added

--- src/components/Nav/Footer/Footer.js
+++ src/components/Nav/Footer/Footer.js
@@ -1,15 +1,48 @@
 import React from 'react'
-import logo from '../../../assets/images/Logo.png'
 import { NavLink } from 'react-router-dom'
 import classes from './Footer.css'
-import { Paper } from '@material-ui/core'
-import { grey } from '@material-ui/core/colors'
+import { Card, Grid, CardMedia, Typography } from '@material-ui/core'
+import { grey, blueGrey } from '@material-ui/core/colors'
+import Logo from '../../../assets/images/Logo.png'
+import { makeStyles } from '@material-ui/core/styles';
 
-const footer = (props) => (
+const useStyles = makeStyles({
+    footer: {
+      backgroundColor: blueGrey['400'],
+    },
+    footerLogo: {
+      height: 140,
+      width: 140
+    },
+  });
+
+
+
+const footer = (props) => {
+    const msClasses = useStyles();
+
+    return(
     <React.Fragment>
-        <footer>
-            <Paper elevation={2} style={{height:'300px', background:grey}}/>
-        </footer>
+        <Card className={classes.Footer} style={{height:'300px'}} >
+            <Grid container justify="center" >
+                <Grid item xs={4}>
+
+                </Grid>
+                <Grid item container justify="center" xs={4}>
+                    <CardMedia
+                        className={msClasses.footerLogo}
+                        image={Logo}
+                    />
+                </Grid>
+                <Grid item xs={4}>
+                    
+                </Grid>
+                <Typography>
+                    Hi, I am the footer
+                </Typography>
+            </Grid>
+            
+        </Card>
                     {/*
                     <br /><NavLink to='/owner-signup'>Register as a Bus Owner</NavLink>
                     <br /><NavLink to='/'>Inquiries</NavLink>
@@ -18,6 +51,7 @@ const footer = (props) => (
                     */ }
                     
     </React.Fragment>
-)
+    )
+}
 
 export default footer;

06f472583239fb2f0aff7676096ba6eb4f1d586d
CharithKularatna <charithkularatna@gmail.com>
2020-05-28 16:42:55 +0530

    Owner Request Table Initiated - Material-table Lib

--- src/containers/RequestPage/RequestPage.js
+++ src/containers/RequestPage/RequestPage.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
+import MaterialTable from "material-table";
+import Divider from '@material-ui/core/Divider'
+import { withStyles } from '@material-ui/core/styles'
+import { Paper } from '@material-ui/core';
 
-export default class RequestPage extends Component {
+const styles = theme => (
+    {
+    
+        table:{
+            marginTop: '25px',
+            maxWidth: '100%',
+            height: '419px',
+            minWidth: '85%',
+        }
+    }
+)
+
+class RequestPage extends Component {
     render(){
+        const {classes} = this.props
         return (
             <React.Fragment>
-                <Typography variant='h6'>Table Comes Here</Typography>
+                <Paper className={classes.table}>
+                    <MaterialTable
+                        style={{overflowY: 'scroll', maxHeight: '90vh'}}
+                        title="Bus Owner Requests"
+                        columns={[
+                            { title: 'Name', field: 'name' },
+                            { title: 'Surname', field: 'surname' },
+                            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                            {
+                            title: 'Birth Place',
+                            field: 'birthCity',
+                            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+                            },
+                        ]}
+                    />
+                </Paper>
+                
             </React.Fragment>
         )
     }
 }
+
+export default withStyles(styles)(RequestPage);
